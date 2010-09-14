9097816d0bfbe527f6016f3b654d3f087716630a
jpdelport <jpdelport@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2010-09-14 20:21:46 +0000

    Overlays: * Add geometry overlays - quad and crosshair * Add example to show overlays and picking

---
+++ trunk/src/flitr/crosshair_overlay.cpp
@@ -0,0 +1,90 @@
+/* Framework for Live Image Transformation (FLITr) 
+ * Copyright (c) 2010 CSIR
+ * 
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <flitr/crosshair_overlay.h>
+
+using namespace flitr;
+
+CrosshairOverlay::CrosshairOverlay(double center_x, double center_y, double width, double height) :
+    GeometryOverlay(),
+    _CenterX(center_x),
+    _CenterY(center_y),
+    _Width(width),
+    _Height(height)
+{
+    _Vertices = new osg::Vec3Array;
+
+    makeCrosshair();
+
+    dirtyBound();
+
+    setColour(osg::Vec4d(1,0,0,0.5));
+}
+
+void CrosshairOverlay::makeCrosshair()
+{
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
+	
+    updateCrosshair();
+
+    geom->setVertexArray(_Vertices.get());
+    
+    osg::ref_ptr<osg::DrawArrays> da;
+    da= new osg::DrawArrays(osg::PrimitiveSet::LINES,0,4);
+
+	geom->addPrimitiveSet(da.get());
+	geom->setUseDisplayList(false);
+	geode->addDrawable(geom.get());
+    geode->setCullingActive(false);
+	
+	_GeometryGroup->addChild(geode.get());
+}
+
+void CrosshairOverlay::updateCrosshair()
+{
+    _Vertices->clear();
+    _Vertices->push_back(osg::Vec3d(_CenterX-(_Width/2), _CenterY, 0));
+    _Vertices->push_back(osg::Vec3d(_CenterX+(_Width/2), _CenterY, 0));
+    _Vertices->push_back(osg::Vec3d(_CenterX, _CenterY+(_Height/2), 0));
+    _Vertices->push_back(osg::Vec3d(_CenterX, _CenterY-(_Height/2), 0));
+    _Vertices->dirty();
+
+    dirtyBound();
+}
+
+void CrosshairOverlay::setCenter(double x, double y)
+{
+    _CenterX = x;
+    _CenterY = y;
+    updateCrosshair();
+}
+
+void CrosshairOverlay::setWidth(double width)
+{
+    _Width = width;
+    updateCrosshair();
+}
+
+void CrosshairOverlay::setHeight(double height)
+{
+    _Height = height;
+    updateCrosshair();
+}
---
+++ trunk/src/flitr/geometry_overlay.cpp
@@ -0,0 +1,98 @@
+/* Framework for Live Image Transformation (FLITr) 
+ * Copyright (c) 2010 CSIR
+ * 
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <flitr/geometry_overlay.h>
+
+using namespace flitr;
+
+GeometryOverlay::GeometryOverlay()
+{
+    getOrCreateStateSet()->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
+    getOrCreateStateSet()->setMode(GL_BLEND,osg::StateAttribute::ON);
+
+    _CoordinateTransform = new osg::MatrixTransform();
+    _DepthTransform = new osg::MatrixTransform();
+    _GeometryGroup = new osg::Group();
+
+    addChild(_DepthTransform.get());
+    _DepthTransform->addChild(_CoordinateTransform.get());
+    _CoordinateTransform->addChild(_GeometryGroup.get());
+    
+    _GeometryMaterial = new osg::Material();
+    _GeometryStateSet = _GeometryGroup->getOrCreateStateSet();
+    _GeometryStateSet->setAttributeAndModes(_GeometryMaterial.get(), osg::StateAttribute::ON);
+    
+    _GeometryLinewidth = new osg::LineWidth();
+    _GeometryLinewidth->setWidth(1);
+    _GeometryStateSet->setAttributeAndModes(_GeometryLinewidth.get(),osg::StateAttribute::ON);
+
+    setColour(osg::Vec4d(1,1,0,0.5));
+
+    setAllChildrenOn();
+
+    setDepth(0.1);
+}
+
+void GeometryOverlay::setDepth(double z)
+{
+    osg::Matrixd d;
+    d.makeTranslate(0,0,z);
+    _DepthTransform->setMatrix(d);
+
+    dirtyBound();
+}
+
+void GeometryOverlay::setLineWidth(double w)
+{
+    _GeometryLinewidth->setWidth(w);
+}
+
+void GeometryOverlay::setColour(osg::Vec4d newcol)
+{
+    _GeometryColour = newcol;
+    _GeometryMaterial->setAmbient(osg::Material::FRONT_AND_BACK, _GeometryColour);
+    _GeometryMaterial->setDiffuse(osg::Material::FRONT_AND_BACK, _GeometryColour);
+}
+
+void GeometryOverlay::setShow(bool show)
+{
+    if (show) {
+        setAllChildrenOn();
+    } else {
+        setAllChildrenOff();
+    }
+}
+
+void GeometryOverlay::setCoordinateTransform(osg::Matrixd m)
+{
+    _CoordinateTransform->setMatrix(m);
+
+    dirtyBound();
+}
+
+void GeometryOverlay::flipVerticalCoordinates(double im_height)
+{
+    osg::Matrixd r;
+    osg::Matrixd t;
+    t.makeTranslate(osg::Vec3d(0,0,im_height));
+    r.makeRotate(osg::DegreesToRadians(180.0), osg::Vec3d(1,0,0));
+    // if viewed from a coordinate's perspective, translate up z and then flip around x
+    setCoordinateTransform(r*t);
+}
