9a772d9b47820600c385ecd9b050537f3779b6d0
bernardt.duvenhage@gmail.com <bernardt.duvenhage@gmail.com@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2012-03-06 07:35:01 +0000

    Added a HUD to which one can attach a geometry overlay. Same as using a geometry overlay on the stream except the HUD overlay geometry is fixed to the screen and connot be panned and scaled.

---
+++ trunk/examples/hud_overlay/hud_overlay.cpp
@@ -0,0 +1,276 @@
+#include <flitr/ffmpeg_producer.h>
+#include <flitr/multi_osg_consumer.h>
+#include <flitr/textured_quad.h>
+#include <flitr/manipulator_utils.h>
+#include <flitr/ortho_texture_manipulator.h>
+#include <flitr/quad_overlay.h>
+#include <flitr/crosshair_overlay.h>
+#include <flitr/hud.h>
+
+#include <osg/Vec3>
+#include <osg/Vec4>
+#include <osg/Quat>
+#include <osg/Matrix>
+
+#include <osgDB/FileUtils>
+#include <osgDB/ReadFile>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+#include <osgGA/TrackballManipulator>
+#include <osg/io_utils>
+
+#include <iostream>
+#include <sstream>
+
+using std::tr1::shared_ptr;
+using namespace flitr;
+
+// Adapted from osgpick example
+class PickHandler : public osgGA::GUIEventHandler {
+  public: 
+    PickHandler() 
+    { 
+      _pick_location_available = false;
+      _pick_size_available = false;
+    }
+
+    ~PickHandler() {}
+
+    enum PickType
+    {
+      LOCATION,
+      SIZE
+    };
+    
+    bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa);
+    virtual void pick(osgViewer::View* view, const osgGA::GUIEventAdapter& ea, PickType pt);
+
+    bool getPickLocation(osg::Vec3d& location)
+    {
+        if (_pick_location_available) {
+            location = _pick_location;
+            _pick_location_available = false;
+            return true;
+        }
+        return false;
+    }
+
+    bool getPickSize(osg::Vec3d& size)
+    {
+        if (_pick_size_available) {
+            size = _pick_size;
+            _pick_size_available = false;
+            return true;
+        }
+        return false;
+    }
+
+  private:
+    bool _pick_location_available;
+    bool _pick_size_available;
+    osg::Vec3d _pick_location;
+    osg::Vec3d _pick_size;
+};
+
+bool PickHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+{
+    unsigned int button_mask=ea.getButtonMask();//GUIEventAdapter::MIDDLE_MOUSE_BUTTON
+    unsigned int mod_key_mask=ea.getModKeyMask();
+    unsigned int event_type=ea.getEventType();
+
+    osgViewer::View* view = dynamic_cast<osgViewer::View*>(&aa);
+
+    if (view)
+    {
+	if (mod_key_mask & osgGA::GUIEventAdapter::MODKEY_SHIFT)
+	{
+	  if (button_mask & osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON)
+	  {
+	    if (event_type==osgGA::GUIEventAdapter::PUSH)
+	    {
+	      pick(view,ea, LOCATION);
+	      
+	    } else
+	    if (event_type==osgGA::GUIEventAdapter::DRAG)
+	    {
+	      pick(view,ea, SIZE);
+	      
+	    }
+	  } else
+	  if (button_mask & osgGA::GUIEventAdapter::RIGHT_MOUSE_BUTTON)
+	  {
+	    if (event_type==osgGA::GUIEventAdapter::PUSH)
+	    {
+	      pick(view,ea, LOCATION);
+	      
+	    } else
+	    if (event_type==osgGA::GUIEventAdapter::DRAG)
+	    {
+	      pick(view,ea, SIZE);
+	      
+	    }
+	  } 
+
+ 	  return true;//Don't send the event to the camera manipulator if shift is held down.
+	}
+    }
+
+    return false;
+}
+
+void PickHandler::pick(osgViewer::View* view, const osgGA::GUIEventAdapter& ea, PickType pt)
+{
+    osgUtil::LineSegmentIntersector::Intersections intersections;
+
+    float x = ea.getX();
+    float y = ea.getY();
+
+    if (view->computeIntersections(x,y,intersections))
+    {
+        for(osgUtil::LineSegmentIntersector::Intersections::iterator hitr = intersections.begin();
+            hitr != intersections.end();
+            ++hitr)
+        {
+            std::ostringstream os;
+            if (!hitr->nodePath.empty() && !(hitr->nodePath.back()->getName().empty()))
+            {
+                // the geodes are identified by name.
+                os<<"Object \""<<hitr->nodePath.back()->getName()<<"\""<<std::endl;
+            }
+            else if (hitr->drawable.valid())
+            {
+                os<<"Object \""<<hitr->drawable->className()<<"\""<<std::endl;
+            }
+
+            if (pt==LOCATION)
+            {
+              _pick_location = hitr->getWorldIntersectPoint();
+              _pick_location_available = true;
+            } else
+            if (pt==SIZE)
+            {
+              _pick_size = hitr->getWorldIntersectPoint() - _pick_location;
+              _pick_size_available = true;
+            }
+
+            os<<"        local coords vertex("<< hitr->getLocalIntersectPoint()<<")"<<"  normal("<<hitr->getLocalIntersectNormal()<<")"<<std::endl;
+            os<<"        world coords vertex("<< hitr->getWorldIntersectPoint()<<")"<<"  normal("<<hitr->getWorldIntersectNormal()<<")"<<std::endl;
+            const osgUtil::LineSegmentIntersector::Intersection::IndexList& vil = hitr->indexList;
+            for(unsigned int i=0;i<vil.size();++i)
+            {
+                os<<"        vertex indices ["<<i<<"] = "<<vil[i]<<std::endl;
+            }
+
+            std::cout << os.str();
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " video_file\n";
+        return 1;
+    }
+
+    shared_ptr<FFmpegProducer> ffp(new FFmpegProducer(argv[1], ImageFormat::FLITR_PIX_FMT_Y_8));
+    if (!ffp->init()) {
+        std::cerr << "Could not load input file.\n";
+        exit(-1);
+    }
+
+    shared_ptr<MultiOSGConsumer> osgc(new MultiOSGConsumer(*ffp,1));
+    if (!osgc->init()) {
+        std::cerr << "Could not init OSG consumer\n";
+        exit(-1);
+    }
+
+    osg::Group *root_node = new osg::Group;
+    
+    shared_ptr<TexturedQuad> quad(new TexturedQuad(osgc->getOutputTexture()));
+    root_node->addChild(quad->getRoot().get());
+
+    osgViewer::Viewer viewer;
+    viewer.setThreadingModel(osgViewer::Viewer::SingleThreaded);
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
+        
+    ImageFormat imf = ffp->getFormat();
+    double w = imf.getWidth();
+    double h = imf.getHeight();
+
+    QuadOverlay* qov = new QuadOverlay(w/2,h/2,w/4,h/4);
+    qov->setLineWidth(2);
+    //qov->flipVerticalCoordinates(h);
+    root_node->addChild(qov);
+
+
+    viewer.setSceneData(root_node);
+    viewer.realize();//Make sure that the camera, etc is set up. Otherwise the HUD overlay can't be set up!!!
+
+
+    OrthoTextureManipulator* om = new OrthoTextureManipulator(w, h);
+    viewer.setCameraManipulator(om);
+
+   //NOTE!!:Make sure to call viewer.realize() such that the camera, etc is set up. Otherwise the HUD overlay can't be set up!!!
+    HUD* hud=new HUD(&viewer, -1.0, 1.0, -1.0, 1.0);
+    hud->setAspectMode(HUD::HUD_ASPECT_FIXED);   
+
+    CrosshairOverlay* ch0 = new CrosshairOverlay(0.0,0.0,0.1,0.1);
+      ch0->setLineWidth(2);
+      ch0->setColour(osg::Vec4d(1.0, 1.0, 1.0, 1.0));
+      hud->addChild(ch0);
+    CrosshairOverlay* ch1 = new CrosshairOverlay(-0.975,-0.975, 0.045,0.045);
+      ch1->setLineWidth(2);
+      ch1->setColour(osg::Vec4d(1.0, 0.0, 0.0, 1.0));
+      hud->addChild(ch1);
+    CrosshairOverlay* ch2 = new CrosshairOverlay(0.975,-0.975, 0.045,0.045);
+      ch2->setLineWidth(2);
+      ch2->setColour(osg::Vec4d(0.0, 1.0, 0.0, 1.0));
+      hud->addChild(ch2);
+    CrosshairOverlay* ch3 = new CrosshairOverlay(0.975,0.975, 0.045,0.045);
+      ch3->setLineWidth(2);
+      ch3->setColour(osg::Vec4d(0.0, 0.0, 1.0, 1.0));
+      hud->addChild(ch3);
+    CrosshairOverlay* ch4 = new CrosshairOverlay(-0.975,0.975, 0.045,0.045);
+      ch4->setLineWidth(2);
+      ch4->setColour(osg::Vec4d(1.0, 1.0, 0.0, 1.0));
+      hud->addChild(ch4);
+
+
+    // disable all culling
+    viewer.getCamera()->setCullingMode(0);
+
+    // add pick handler
+    PickHandler* ph = new PickHandler; 
+    viewer.addEventHandler(ph);
+
+    while(!viewer.done())
+    {
+        ffp->trigger();
+
+        if (osgc->getNext()) {
+            viewer.frame();
+
+            osg::Vec3d pickLocation;
+            if (ph->getPickLocation(pickLocation)) {
+                //ortho_manip->setCenter(pickpoint);
+                qov->setCenter(pickLocation.x(), pickLocation.y());
+            }
+
+            osg::Vec3d pickSize;
+            if (ph->getPickSize(pickSize)) {
+                qov->setWidth(pickSize.x()*2.0);
+                qov->setHeight(pickSize.y()*2.0);
+            }
+
+        } else {
+            OpenThreads::Thread::microSleep(5000);
+        }
+    }
+
+    return 0;
+}
---
+++ trunk/include/flitr/hud.h
@@ -0,0 +1,231 @@
+/* Framework for Live Image Transformation (FLITr) 
+ * Copyright (c) 2010 CSIR
+ * 
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FLITR_HUD_H
+#define FLITR_HUD_H 1
+
+#include <flitr/flitr_export.h>
+
+#include <osg/Camera>
+#include <osgGA/GUIEventHandler>
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+
+namespace flitr {
+
+/**
+ * A hud that spans the viewport.
+ */
+class FLITR_EXPORT HUD : public osg::Camera
+{
+  public:
+
+    enum AnchorMode
+    {
+	HUD_ANCHOR_CENTER = 0,
+        HUD_ANCHOR_BOTTOM_LEFT = 1
+    };
+
+    enum AspectMode
+    {
+        HUD_ASPECT_FIXED = 0, //Always show whole HUD in view.
+        HUD_ASPECT_FIXED_FILL_X = 1, //Fill view in width with HUD.
+        HUD_ASPECT_FIXED_FILL_Y = 2, //Fill view in height with HUD.
+        HUD_ASPECT_STRETCHED = 3, //Stretch HUD to fill view in WIDTH and HEIGHT.
+        HUD_ASPECT_TRACK_VIEW = 4, //Keep aspect of HUD the same as the view with dimensions in pixels.
+    };
+
+  private:
+
+        /**
+         * The resize handler makes sure that the HUD is resized when
+         * the view it is attached to changes its size.
+         * 
+         */
+        struct ResizeHandler : public osgGA::GUIEventHandler
+        {
+            ResizeHandler(HUD *hud) :
+                hud_(hud),
+                old_view_width_(-1),
+                old_view_height_(-1)
+            {}
+            
+            bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter&)
+            {
+                if (ea.getHandled()) return false;
+                
+                switch(ea.getEventType())
+                {
+                  case(osgGA::GUIEventAdapter::FRAME):
+                  {
+                      double view_width = hud_->getViewport()->width();//In pixels.
+                      double view_height = hud_->getViewport()->height();//In pixels.
+
+                      if ( (view_height!=0) &&
+                           ((view_width != old_view_width_) ||
+                            (view_height != old_view_height_))
+                         )
+                      {
+                          old_view_width_ = view_width;
+                          old_view_height_ = view_height;
+                          
+                          double view_aspect = view_width/view_height;
+						  double ratio_change = view_aspect / hud_->aspect_;
+						  
+						  // The default choice is a stretched HUD; HUD_ASPECT_STRETCHED.
+						  osg::Matrix proj = osg::Matrix::ortho2D(hud_->min_x_,
+																  hud_->max_x_,
+																  hud_->min_y_,
+																  hud_->max_y_);
+						  
+						  switch (hud_->aspect_mode_) {
+                            case HUD_ASPECT_TRACK_VIEW: {
+                                // Keep aspect of HUD the same as the view with dimensions in pixels.
+                                hud_->setMinMax(0, view_width, 0, view_height);
+                                proj = osg::Matrix::ortho2D(0, view_width, 0, view_height);
+                                break;
+                            }
+                            case HUD_ASPECT_FIXED: {
+                                //Always show whole HUD in view.
+                                if (view_aspect > hud_->aspect_) {
+                                    proj *= osg::Matrix::scale(1.0/ratio_change, 1.0, 1.0);
+								  
+                                    if (hud_->anchor_mode_ == HUD_ANCHOR_BOTTOM_LEFT) {
+                                        double w = hud_->width_;
+                                        double shift = -0.5*(w - (w/ratio_change));
+                                        osg::Matrixd shift_m;
+                                        shift_m.makeTranslate(osg::Vec3d(shift, 0.0, 0.0));
+                                        proj*=shift_m;
+                                    }
+                                } else {
+                                    proj *= osg::Matrix::scale(1.0, ratio_change, 1.0);
+								  
+                                    if (hud_->anchor_mode_ == HUD_ANCHOR_BOTTOM_LEFT) {
+                                        double h = hud_->height_;
+                                        double shift = -0.5*(h - (h*ratio_change));
+                                        osg::Matrixd shift_m;
+                                        shift_m.makeTranslate(osg::Vec3d(0.0, shift, 0.0));
+                                        proj*=shift_m;
+                                    }
+                                }
+                                break;
+                            }
+                            case HUD_ASPECT_FIXED_FILL_X: {
+                                // Fill view in width with HUD.
+                                proj *= osg::Matrix::scale(1.0, ratio_change, 1.0);
+							  
+                                if (hud_->anchor_mode_ == HUD_ANCHOR_BOTTOM_LEFT) {
+                                    double h = hud_->height_;
+                                    double shift = -0.5*(h - (h*ratio_change));
+                                    osg::Matrixd shift_m;
+                                    shift_m.makeTranslate(osg::Vec3d(0.0, shift, 0.0));
+                                    proj*=shift_m;
+                                }
+                                break;
+                            }
+                            case HUD_ASPECT_FIXED_FILL_Y: {
+                                // Fill view in height with HUD.
+                                proj *= osg::Matrix::scale(1.0/ratio_change, 1.0, 1.0);
+							  
+                                if (hud_->anchor_mode_ == HUD_ANCHOR_BOTTOM_LEFT) {
+                                    double w = hud_->width_;
+                                    double shift = -0.5*(w - (w/ratio_change));
+                                    osg::Matrixd shift_m;
+                                    shift_m.makeTranslate(osg::Vec3d(shift, 0.0, 0.0));
+                                    proj*=shift_m;
+                                }
+                                break;
+                            }
+                            default:
+                              break;
+                          }
+						  
+						  hud_->getProjectionMatrix() = proj;
+					  }
+					  
+                      return false;
+                  }
+                  default:
+                    break;
+                }
+				
+                return false;
+            }
+			
+            HUD* hud_;
+			
+            double old_view_width_;//In pixels.
+            double old_view_height_;//In pixels.
+        };
+        
+        friend struct ResizeHandler;
+		
+  public:
+    HUD(osgViewer::View *view, const double minX=0.0, const double maxX=1.0, const double minY=0.0, const double maxY=1.0);
+   ~HUD();
+
+    osgGA::GUIEventHandler* getResizeHandler()
+    {
+      return resize_handler_.get();
+    }
+
+    void setAnchorMode(const AnchorMode m) { anchor_mode_ = m; }
+		
+    void setAspectMode(const AspectMode m) { aspect_mode_ = m; }
+
+    void setMinMax(const double minX, const double maxX, const double minY, const double maxY)
+    {
+            if (maxX <= minX) return;
+            if (maxY <= minY) return;
+
+			min_x_ = minX;
+			max_x_ = maxX;
+			
+			min_y_ = minY;
+			max_y_ = maxY;
+			
+			width_ = max_x_ - min_x_;
+			height_ = max_y_ - min_y_;
+			
+			aspect_ = width_/height_;
+    }
+
+
+  private:
+        osg::ref_ptr<ResizeHandler> resize_handler_;
+		
+        double min_x_;
+        double max_x_;
+        double width_;
+		
+        double min_y_;
+        double max_y_;
+        double height_;
+		
+        double aspect_;
+		
+        AnchorMode anchor_mode_;
+        AspectMode aspect_mode_;
+};
+
+}
+
+#endif //FLITR_HUD
+
