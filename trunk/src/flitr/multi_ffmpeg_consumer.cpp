b205d0a5e9a6bb747e5b48f80efe39cb5d15ff19
bernardt.duvenhage@gmail.com <bernardt.duvenhage@gmail.com@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2012-08-13 07:07:35 +0000

    Updated the consumer to allow the writing of image streams within a slot to be disabled. An image stream is enabled when it has a non-empty file name and disabled otherwise. At the moment the consumer will not work correctly when all the image streams are disabled.

--- trunk/src/flitr/multi_ffmpeg_consumer.cpp
+++ trunk/src/flitr/multi_ffmpeg_consumer.cpp
@@ -1,18 +1,18 @@
 /* Framework for Live Image Transformation (FLITr) 
  * Copyright (c) 2010 CSIR
- * 
+ *
  * This file is part of FLITr.
  *
  * FLITr is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * FLITr is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with FLITr. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -36,9 +36,13 @@ void MultiFFmpegConsumerThread::run()
             OpenThreads::ScopedLock<OpenThreads::Mutex> wlock(Consumer_->WritingMutex_);
             if (Consumer_->Writing_) {
                 for (unsigned int i=0; i<num_writers; i++) {
-                    Image* im = *(imv[i]);
-                    Consumer_->FFmpegWriters_[i]->writeVideoFrame(im->data());
-                    Consumer_->MetadataWriters_[i]->writeFrame(*im);
+                    if (Consumer_->FFmpegWriters_[i])
+                    {
+                        Image* im = *(imv[i]);
+
+                        Consumer_->FFmpegWriters_[i]->writeVideoFrame(im->data());
+                        Consumer_->MetadataWriters_[i]->writeFrame(*im);
+                    }
                 }
             } else {
                 // just discard
@@ -124,11 +128,18 @@ bool MultiFFmpegConsumer::openFiles(std::vector<std::string> filenames, const ui
     {
         for (unsigned int i=0; i<ImagesPerSlot_; i++)
         {
-            std::string video_filename(filenames[i] + ".avi");
-            std::string metadata_filename(filenames[i] + ".meta");
+            if (filenames[i]!="")
+            {
+                std::string video_filename(filenames[i] + ".avi");
+                std::string metadata_filename(filenames[i] + ".meta");
 
-            FFmpegWriters_[i] = new FFmpegWriter(video_filename, ImageFormat_[i], frame_rate);
-            MetadataWriters_[i] = new MetadataWriter(metadata_filename);
+                FFmpegWriters_[i] = new FFmpegWriter(video_filename, ImageFormat_[i], frame_rate);
+                MetadataWriters_[i] = new MetadataWriter(metadata_filename);
+            } else
+            {//If the filename is "" then the recording is disbaled.
+                FFmpegWriters_[i] = 0;
+                MetadataWriters_[i] = 0;
+            }
         }
         return true;
     } else
