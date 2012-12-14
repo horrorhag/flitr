996e0b665c6aab3b3418f268c2ad21798dad1332
bernardt.duvenhage <bduvenhage@users.noreply.github.com>
2012-12-14 08:38:11 +0000

    Framework of de-motion-blur flitr example for Ritesh TheStudent to work on.

---
+++ examples/de_motion_blur/de_motion_blur_example.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <string>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgGA/TrackballManipulator>
+#include <osg/io_utils>
+
+#include <flitr/modules/de_motion_blur/de_motion_blur.h>
+#include <flitr/ffmpeg_producer.h>
+#include <flitr/multi_ffmpeg_consumer.h>
+#include <flitr/multi_osg_consumer.h>
+#include <flitr/textured_quad.h>
+#include <flitr/manipulator_utils.h>
+#include <flitr/ortho_texture_manipulator.h>
+
+using std::tr1::shared_ptr;
+using namespace flitr;
+
+class BackgroundTriggerThread : public OpenThreads::Thread {
+public:
+    BackgroundTriggerThread(ImageProducer* p) :
+        Producer_(p),
+        ShouldExit_(false),
+        ReadableTarget_(5) {}
+
+    void run()
+    {
+        while(!ShouldExit_) {
+            bool triggerred = false;
+            while (Producer_->getLeastNumReadSlotsAvailable() < ReadableTarget_) {
+                Producer_->trigger();
+                triggerred = true;
+            }
+            if (!triggerred) {
+                Thread::microSleep(5000);
+            }
+        }
+    }
+
+    void setExit() { ShouldExit_ = true; }
+
+private:
+    ImageProducer* Producer_;
+    bool ShouldExit_;
+    const uint32_t ReadableTarget_;
+};
+
+//#define USE_BACKGROUND_TRIGGER_THREAD 1
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " video_file\n";
+        return 1;
+    }
+
+    shared_ptr<FFmpegProducer> ffp(new FFmpegProducer(argv[1], ImageFormat::FLITR_PIX_FMT_Y_8));
+    if (!ffp->init()) {
+        std::cerr << "Could not load " << argv[1] << "\n";
+        exit(-1);
+    }
+
+#ifdef USE_BACKGROUND_TRIGGER_THREAD
+    shared_ptr<BackgroundTriggerThread> btt(new BackgroundTriggerThread(ffp.get()));
+    btt->startThread();
+#endif
+
+    shared_ptr<DeMotionBlur> deMotionBlur(new DeMotionBlur(*ffp, 1));
+    if (!deMotionBlur->init()) {
+        std::cerr << "Could not initialise the target injector.\n";
+        exit(-1);
+    }
+
+
+    deMotionBlur->startTriggerThread();
+
+
+    shared_ptr<MultiOSGConsumer> osgc(new MultiOSGConsumer(*deMotionBlur,1));
+    if (!osgc->init()) {
+        std::cerr << "Could not init OSG consumer\n";
+        exit(-1);
+    }
+
+    shared_ptr<MultiFFmpegConsumer> mffc(new MultiFFmpegConsumer(*deMotionBlur,1));
+    if (!mffc->init()) {
+        std::cerr << "Could not init FFmpeg consumer\n";
+        exit(-1);
+    }
+    std::stringstream filenameStringStream;
+    filenameStringStream << argv[1] << "_ti";
+    mffc->openFiles(filenameStringStream.str());
+    mffc->startWriting();
+
+    osg::Group *root_node = new osg::Group;
+    
+    shared_ptr<TexturedQuad> quad(new TexturedQuad(osgc->getOutputTexture()));
+    root_node->addChild(quad->getRoot().get());
+
+    osgViewer::Viewer viewer;
+    viewer.setThreadingModel(osgViewer::Viewer::SingleThreaded);
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.setSceneData(root_node);
+
+    //viewer.setUpViewInWindow(480+40, 40, 640, 480);
+    viewer.realize();
+
+    const int use_trackball = 0;
+    if (use_trackball) {
+        osgGA::TrackballManipulator* tb = new osgGA::TrackballManipulator;
+        viewer.setCameraManipulator(tb);
+        adjustCameraManipulatorHomeForYUp(tb);
+    } else {
+
+        ImageFormat imf = ffp->getFormat();
+        OrthoTextureManipulator* om = new OrthoTextureManipulator(imf.getWidth(), imf.getHeight());
+        viewer.setCameraManipulator(om);
+    }
+
+    while((!viewer.done())&&(ffp->getCurrentImage()<(ffp->getNumImages()*0.9f))) {
+#ifndef USE_BACKGROUND_TRIGGER_THREAD
+        //Read from the video, but don't get more than 10 frames ahead.
+        if (ffp->getLeastNumReadSlotsAvailable()<10)
+        {
+            ffp->trigger();
+        }
+#endif
+
+        if (!(deMotionBlur->isTriggerThreadStarted()))
+        {
+            deMotionBlur->trigger();
+        }
+
+        if (osgc->getNext()) {
+            viewer.frame();
+        }
+    }
+
+    mffc->stopWriting();
+    mffc->closeFiles();
+
+#ifdef USE_BACKGROUND_TRIGGER_THREAD
+    btt->setExit();
+    btt->join();
+#endif   
+
+    return 0;
+}
---
+++ src/flitr/modules/de_motion_blur/de_motion_blur.cpp
@@ -0,0 +1,114 @@
+/* Framework for Live Image Transformation (FLITr)
+ * Copyright (c) 2010 CSIR
+ *
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <flitr/modules/de_motion_blur/de_motion_blur.h>
+
+using namespace flitr;
+using std::tr1::shared_ptr;
+
+DeMotionBlur::DeMotionBlur(ImageProducer& producer,
+                               uint32_t images_per_slot, uint32_t buffer_size) :
+    ImageProcessor(producer, images_per_slot, buffer_size)
+{
+}
+
+DeMotionBlur::~DeMotionBlur()
+{
+}
+
+bool DeMotionBlur::init()
+{
+    bool rValue=ImageProcessor::init();
+
+    return rValue;
+}
+
+bool DeMotionBlur::trigger()
+{
+    if ((getNumReadSlotsAvailable())&&(getNumWriteSlotsAvailable()))
+    {//There are images to consume and the downstream producer has space to produce.
+        std::vector<Image**> imvRead=reserveReadSlot();
+        std::vector<Image**> imvWrite=reserveWriteSlot();
+
+        //Start stats measurement event.
+        ProcessorStats_->tick();
+
+
+        unsigned int i=0;
+        for (i=0; i<ImagesPerSlot_; i++)
+        {
+            Image const * const imRead = *(imvRead[i]);
+            Image * const imWrite = *(imvWrite[i]);
+            const ImageFormat imFormat=getFormat(i);
+            const ImageFormat::PixelFormat pixelFormat=imFormat.getPixelFormat();
+
+            const uint32_t width=imFormat.getWidth();
+            const uint32_t height=imFormat.getHeight();
+            const uint32_t bytesPerPixel=imFormat.getBytesPerPixel();
+            uint8_t const * const dataRead=imRead->data();
+            uint8_t * const dataWrite=imWrite->data();
+
+            uint32_t offset=0;
+
+
+            //Do image processing here...
+
+
+            #pragma omp parallel for
+            for (uint32_t y=0; y<height; y++)
+            {
+                for (uint32_t x=0; x<width; x++)
+                {
+                    switch (pixelFormat)
+                    {
+                    case ImageFormat::FLITR_PIX_FMT_Y_8 :
+                        dataWrite[offset]=(uint8_t)(dataRead[offset] * 128);
+                        break;
+                    case ImageFormat::FLITR_PIX_FMT_RGB_8 :
+                        dataWrite[offset]=(uint8_t)(dataRead[offset]);
+                        dataWrite[offset+1]=(uint8_t)(dataRead[offset+1]);
+                        dataWrite[offset+2]=(uint8_t)(dataRead[offset+2]);
+                        break;
+                    case ImageFormat::FLITR_PIX_FMT_Y_16 :
+                        dataWrite[offset]=(uint8_t)(dataRead[offset]);
+                        dataWrite[offset+1]=(uint8_t)(dataRead[offset+1]);
+                        break;
+                        //default:
+                    }
+
+                    offset+=bytesPerPixel;
+                }
+            }
+
+
+        }
+
+        //Stop stats measurement event.
+        ProcessorStats_->tock();
+
+        releaseWriteSlot();
+        releaseReadSlot();
+
+        return true;
+    }
+
+    return false;
+}
+
