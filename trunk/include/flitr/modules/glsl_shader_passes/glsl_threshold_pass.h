be77e2760081f1817446dc7611d18f112854ba01
natalie.ausmeier@gmail.com <natalie.ausmeier@gmail.com@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2013-10-24 10:19:37 +0000

    Added glsl threshold pass.

---
+++ trunk/include/flitr/modules/glsl_shader_passes/glsl_threshold_pass.h
@@ -0,0 +1,112 @@
+/* Framework for Live Image Transformation (FLITr)
+ * Copyright (c) 2010 CSIR
+ *
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GLSL_THRESHOLD_PASS_H
+#define GLSL_THRESHOLD_PASS_H 1
+
+#include <iostream>
+#include <string>
+
+#include <flitr/flitr_export.h>
+#include <flitr/modules/parameters/parameters.h>
+#include <flitr/texture.h>
+
+
+#include <osg/ref_ptr>
+#include <osg/Group>
+#include <osg/Camera>
+#include <osg/MatrixTransform>
+#include <osg/Projection>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/Texture2D>
+#include <osg/TextureRectangle>
+#include <osg/Image>
+
+namespace flitr {
+
+class FLITR_EXPORT GLSLThresholdPass : public Parameters
+{
+  public:
+    GLSLThresholdPass(flitr::TextureRectangle *in_tex, bool read_back_to_CPU = false);
+    ~GLSLThresholdPass();
+
+    osg::ref_ptr<osg::Group> getRoot() { return RootGroup_; }
+    osg::ref_ptr<flitr::TextureRectangle> getOutputTexture() { return OutTexture_; }
+    osg::ref_ptr<osg::Image> getOSGImage() { return OutImage_; }
+
+    void setThreshold(int value);
+    float getThreshold() const;
+
+
+    virtual int getNumberOfParms()
+    {
+        return 1;
+    }
+
+    virtual EParmType getParmType(int id)
+    {
+        return PARM_INT;
+    }
+
+    virtual std::string getParmName(int id)
+    {
+        return std::string("threshold");
+    }
+
+    virtual std::string getTitle()
+    {
+        return "Threshold Pass";
+    }
+
+    virtual int getInt(int id)
+    {
+        return getThreshold();
+    }
+
+    virtual bool setInt(int id, float v)
+    {
+        setThreshold(v);
+        return true;
+    }
+
+  private:
+    void setShader();
+
+    osg::ref_ptr<osg::Group> createTexturedQuad();
+    void createOutputTexture(bool read_back_to_CPU);
+    void setupCamera();
+
+    osg::ref_ptr<osg::Group> RootGroup_;
+    osg::ref_ptr<osg::Camera> Camera_;
+    osg::ref_ptr<flitr::TextureRectangle> InTexture_;
+    osg::ref_ptr<flitr::TextureRectangle> OutTexture_;
+    osg::ref_ptr<osg::Image> OutImage_;
+
+    int TextureWidth_;
+    int TextureHeight_;
+
+	osg::ref_ptr<osg::Program> FragmentProgram_;
+    osg::ref_ptr<osg::StateSet> StateSet_;
+
+	osg::ref_ptr<osg::Uniform> UniformThreshold_;
+};
+}
+#endif //GLSL_THRESHOLD_PASS_H
