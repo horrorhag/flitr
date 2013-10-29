fab99969ae4e6b7f80a7392b4a9fba2f2d62baec
natalie.ausmeier <HorrorHag@users.noreply.github.com>
2013-10-29 13:19:36 +0000

    Bug fixes regarding bounding rectangle intersections.

--- src/flitr/modules/cpu_shader_passes/cpu_find_discreet_objects_pass.cpp
+++ src/flitr/modules/cpu_shader_passes/cpu_find_discreet_objects_pass.cpp
@@ -44,6 +44,7 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 
     const unsigned long width=Image_->s();
     const unsigned long height=Image_->t();
+	int size = width*height;
 
     unsigned char * const data=(unsigned char *)Image_->data();
 	if (!temp)
@@ -78,7 +79,7 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 		for (unsigned long h=0; h<height; h++)
 		{
 			i = (h*width) + w;
-			if (temp[i] == 0 || (std::find(used.begin(), used.end(), i)!=used.end()) )
+			if (i >= size || i < 0 || temp[i] == 0 || (std::find(used.begin(), used.end(), i)!=used.end()) )
 				continue;
 			FollowBorder(idx, width, height, w, h);
 			idx++;
@@ -88,7 +89,7 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 	// bounding rectangles
 	for (i=0; i<(int)borders.size(); i++)
 	{
-		int left = width, top = height, bottom = 0, right = 0;
+		int left = width, bottom = 0, right = 0, top = height;
 		std::vector< std::pair<int, int> > border = borders[i];
 		for(int j=0; j < (int)border.size(); j++)
 		{
@@ -96,23 +97,23 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 			int h = border[j].second;
 			if (w < left) left = w;
 			if (w > right) right = w;
-			if (h < top) top = h;
 			if (h > bottom) bottom = h;
+			if (h < top) top = h;
 		}
 		if (border.size()>0)
 		{
-			int growV = (right - left) * expandRects_;
-			int growH = (bottom - top) * expandRects_;
+			int growV = ((float)(right - left) * expandRects_) + 0.5f;
+			int growH = ((float)(bottom - top) * expandRects_) + 0.5f;
 			
 			left-=growV;
 			right+=growV;
-			top-=growV;
 			bottom+=growH;
+			top-=growH;
 
 			if (left < 0) left = 0;
-			if (right >= width) right = width-1;
+			if (right >= (int)width) right = width-1;
 			if (top < 0) top = 0;
-			if (bottom >= height) bottom = height-1;
+			if (bottom >= (int)height) bottom = height-1;
 
 			rectangles.push_back(Rect(left, right, top, bottom));
 		}
@@ -122,6 +123,7 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 	bool intersect = true;
 	
 	Rect r1,r2;
+	int left, top, bottom, right;
 	while (intersect)
 	{
 		if (rectangles.size() < 2) intersect = false;
@@ -129,14 +131,11 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 		{
 			for (int j = i+1; j < (int)rectangles.size(); j++)
 			{
-				intersect = (Intersect(rectangles[i], rectangles[j]) || Intersect(rectangles[j], rectangles[i]));
+				r1 = rectangles[i];
+				r2 = rectangles[j];
+				intersect = (Intersect(r1, r2));
 				if (intersect)
 				{
-					int left, top, bottom, right;
-				
-					r1 = rectangles[i];
-					r2 = rectangles[j];
-
 					if (r1.left < r2.left) 
 						left = r1.left;
 					else
@@ -145,43 +144,42 @@ void CPUFindDiscreetObjectsPass::operator()(osg::RenderInfo& renderInfo) const
 						right = r1.right;
 					else
 						right = r2.right;
-					if (r1.top < r2.top)
-						top = r1.top;
-					else
-						top = r2.top;
 					if (r1.bottom > r2.bottom)
 						bottom = r1.bottom;
 					else
 						bottom = r2.bottom;
+					if (r1.top < r2.top)
+						top = r1.top;
+					else
+						top = r2.top;
 
 					rectangles.push_back(Rect(left, right, top, bottom));
-					rectangles.erase(rectangles.begin()+i);
 					rectangles.erase(rectangles.begin()+j);
+					rectangles.erase(rectangles.begin()+i);
 					break;
 				}
 				if (intersect)
 					break;
 			}
+			if (intersect)
+				break;
 		}
 	}
 
 	// Min and Max checks
 	Rect r;
-	int area, w, h;
+	int area;
 	for (i = rectangles.size()-1; i >= 0 ; i--)
 	{
 		r = rectangles[i];
-		w = r.right-r.left;
-		h = r.bottom-r.top;
-		area = w * h;
+		area = r.width * r.height;
 
-		if (area < minArea_ || w < minWidth_ || h < minHeight_ || area > maxArea_ || w > maxWidth_ || h > maxHeight_)
+		if (area < minArea_ || r.width < minWidth_ || r.height < minHeight_ || area > maxArea_ || r.width > maxWidth_ || r.height > maxHeight_)
 		{
 			rectangles.erase(rectangles.begin()+i);
 		}
 	}
 	
-
 	// draw edges
 	for (i=0; i < (int)(width*height); i++)
 	{
@@ -205,9 +203,7 @@ void FollowBorder(int idx, int width, int height, int w, int h)
 		dh = h+delta[l][0];
 		dw = w+delta[l][1];
 		i = (dh*width) + dw;
-		if (i >= size)
-			continue;
-		if (temp[i] == 0 || (std::find(used.begin(), used.end(), i)!=used.end()) )
+		if (i >= size || i < 0 || temp[i] == 0 || (std::find(used.begin(), used.end(), i)!=used.end()) )
 			continue;
 		used.push_back(i);
 		borders[idx].push_back(std::pair<int,int>(dw,dh));
@@ -217,9 +213,7 @@ void FollowBorder(int idx, int width, int height, int w, int h)
 
 bool Intersect(CPUFindDiscreetObjectsPass::Rect r1, CPUFindDiscreetObjectsPass::Rect r2)
 {
-	return ( (r1.left >= r2.left && r1.left <= r2.right) && (r1.top >= r2.top && r1.top <= r2.bottom)
-		|| (r1.right >= r2.left && r1.right <= r2.right) && (r1.top >= r2.top && r1.top <= r2.bottom)
-		|| (r1.left >= r2.left && r1.left <= r2.right) && (r1.bottom >= r2.top && r1.bottom <= r2.bottom)
-		|| (r1.right >= r2.left && r1.right <= r2.right) && (r1.bottom >= r2.top && r1.bottom <= r2.bottom)
-		);
+	return ( (abs(r1.centerX - r2.centerX) <= (r1.width/2 + r2.width/2) )
+		&& ( abs(r1.centerY - r2.centerY) <= (r1.height/2 + r2.height/2)) );
+
 }
