d17e7008e9847ef87ac49aba115c4ee2e71f3cd5
bernardt.duvenhage <bernardt.duvenhage@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2014-03-11 08:06:13 +0000

    1) Added code to retrieve image stabilisation info. 2) Put back code to save standard deviation info. 3) Added a float image crop pass.

---
+++ trunk/include/flitr/modules/flitr_image_processors/crop/fip_crop.h
@@ -0,0 +1,64 @@
+/* Framework for Live Image Transformation (FLITr)
+ * Copyright (c) 2010 CSIR
+ *
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FIP_CROP_H
+#define FIP_CROP_H 1
+
+#include <flitr/image_processor.h>
+
+namespace flitr {
+    
+    /* Applies a power law tone mapping to the image. */
+    class FLITR_EXPORT FIPCrop : public ImageProcessor
+    {
+    public:
+        
+        /*! Constructor given the upstream producer.
+         *@param producer The upstream image producer.
+         *@param images_per_slot The number of images per image slot from the upstream producer.
+         *@param buffer_size The size of the shared image buffer of the downstream producer.*/
+        FIPCrop(ImageProducer& upStreamProducer, uint32_t images_per_slot,
+                size_t startX,
+                size_t startY,
+                size_t width,
+                size_t height,
+                uint32_t buffer_size=FLITR_DEFAULT_SHARED_BUFFER_NUM_SLOTS);
+        
+        /*! Virtual destructor */
+        virtual ~FIPCrop();
+        
+        /*! Method to initialise the object.
+         *@return Boolean result flag. True indicates successful initialisation.*/
+        virtual bool init();
+        
+        /*!Synchronous trigger method. Called automatically by the trigger thread if started.
+         *@sa ImageProcessor::startTriggerThread*/
+        virtual bool trigger();
+        
+    private:
+        size_t startX_;
+        size_t startY_;
+        
+        size_t width_;
+        size_t height_;
+    };
+}
+
+#endif //FIP_CROP_H
