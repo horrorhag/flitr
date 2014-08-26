6483af2789612fbf2d5c1db40bc106a8f59f5800
bernardt.duvenhage <bernardt.duvenhage@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2014-08-26 07:37:44 +0000

    Experimental DPT implementation.

---
+++ trunk/examples/DPT/dpt.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <string>
+
+#include <osgViewer/Viewer>
+#include <osgViewer/ViewerEventHandlers>
+#include <osgGA/TrackballManipulator>
+#include <osg/io_utils>
+
+#include <flitr/modules/geometry_overlays/points_overlay.h>
+#include <flitr/modules/flitr_image_processors/dpt/fip_dpt.h>
+
+#include <flitr/ffmpeg_producer.h>
+#include <flitr/test_pattern_producer.h>
+#include <flitr/multi_ffmpeg_consumer.h>
+#include <flitr/multi_osg_consumer.h>
+#include <flitr/textured_quad.h>
+#include <flitr/manipulator_utils.h>
+#include <flitr/ortho_texture_manipulator.h>
+
+using std::shared_ptr;
+using namespace flitr;
+
+class BackgroundTriggerThread : public OpenThreads::Thread {
+public:
+    BackgroundTriggerThread(ImageProducer* p) :
+    Producer_(p),
+    ShouldExit_(false),
+    ReadableTarget_(5) {}
+    
+    void run()
+    {
+        while(!ShouldExit_) {
+            bool triggerred = false;
+            while (Producer_->getLeastNumReadSlotsAvailable() < ReadableTarget_) {
+                Producer_->trigger();
+                triggerred = true;
+            }
+            Thread::microSleep(5000);
+        }
+    }
+    
+    void setExit() { ShouldExit_ = true; }
+    
+private:
+    ImageProducer* Producer_;
+    bool ShouldExit_;
+    const uint32_t ReadableTarget_;
+};
+
+
+#define USE_BACKGROUND_TRIGGER_THREAD 1
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " video_file filter_pulse_size\n";
+        return 1;
+    }
+    
+    shared_ptr<FFmpegProducer> ip(new FFmpegProducer(argv[1], ImageFormat::FLITR_PIX_FMT_Y_8));
+    if (!ip->init()) {
+        std::cerr << "Could not load " << argv[1] << "\n";
+        exit(-1);
+    }
+    
+#ifdef USE_BACKGROUND_TRIGGER_THREAD
+    shared_ptr<BackgroundTriggerThread> btt(new BackgroundTriggerThread(ip.get()));
+    btt->startThread();
+#endif
+    
+    shared_ptr<FIPDPT> dpt(new FIPDPT(*ip, atoi(argv[2]), 1, 2));
+    if (!dpt->init()) {
+        std::cerr << "Could not initialise the dpt processor.\n";
+        exit(-1);
+    }
+    dpt->startTriggerThread();
+    
+    shared_ptr<MultiOSGConsumer> osgc(new MultiOSGConsumer(*dpt, 1, 2));
+    if (!osgc->init()) {
+        std::cerr << "Could not init OSG consumer\n";
+        exit(-1);
+    }    
+    
+    shared_ptr<MultiOSGConsumer> osgcOrig(new MultiOSGConsumer(*ip, 1));
+    if (!osgcOrig->init()) {
+        std::cerr << "Could not init osgcOrig consumer\n";
+        exit(-1);
+    }
+    
+    
+    /*
+     shared_ptr<MultiFFmpegConsumer> mffc(new MultiFFmpegConsumer(*cnvrtToM8,1));
+     if (!mffc->init()) {
+     std::cerr << "Could not init FFmpeg consumer\n";
+     exit(-1);
+     }
+     std::stringstream filenameStringStream;
+     filenameStringStream << argv[1] << "_improved";
+     mffc->openFiles(filenameStringStream.str());
+     */
+    
+    osg::Group *root_node = new osg::Group;
+    
+    shared_ptr<TexturedQuad> quad(new TexturedQuad(osgc->getOutputTexture()));
+    root_node->addChild(quad->getRoot().get());
+    {
+        osg::Matrix translate;
+        translate.makeTranslate(osg::Vec3d(osgcOrig->getOutputTexture()->getTextureWidth()/2,
+                                           0.0,
+                                           0.0));
+        quad->setTransform(translate);
+    }
+    
+    shared_ptr<TexturedQuad> quadOrig(new TexturedQuad(osgcOrig->getOutputTexture()));
+    root_node->addChild(quadOrig->getRoot().get());
+    {
+        osg::Matrix translate;
+        translate.makeTranslate(osg::Vec3d(-osgcOrig->getOutputTexture()->getTextureWidth()/2,
+                                           0.0,
+                                           0.0));
+        quadOrig->setTransform(translate);
+    }
+    
+    
+    osgViewer::Viewer viewer;
+    viewer.setThreadingModel(osgViewer::Viewer::SingleThreaded);
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.setSceneData(root_node);
+    
+    viewer.setUpViewInWindow(100, 100, 640, 480);
+    viewer.realize();
+    
+        OrthoTextureManipulator* om = new OrthoTextureManipulator(osgc->getOutputTexture()->getTextureWidth(), osgc->getOutputTexture()->getTextureHeight());
+        viewer.setCameraManipulator(om);
+    
+    size_t numFrames=0;
+    while((!viewer.done())/*&&(ffp->getCurrentImage()<(ffp->getNumImages()*0.9f))*/)
+    {
+#ifndef USE_BACKGROUND_TRIGGER_THREAD
+        //Read from the video, but don't get more than n frames ahead.
+        if (ip->getLeastNumReadSlotsAvailable()<5)
+        {
+            ip->trigger();
+        }
+#endif
+        
+        if (osgc->getNext())
+        {
+            osgcOrig->getNext();
+            
+            viewer.frame();
+            numFrames++;
+        }
+        
+        OpenThreads::Thread::microSleep(1000);
+    }
+    
+    //     mffc->stopWriting();
+    //     mffc->closeFiles();
+    
+    
+#ifdef USE_BACKGROUND_TRIGGER_THREAD
+    btt->setExit();
+    btt->join();
+#endif
+    
+    dpt->stopTriggerThread();
+    
+    return 0;
+}
---
+++ trunk/include/flitr/modules/flitr_image_processors/dpt/fip_dpt.h
@@ -0,0 +1,194 @@
+/* Framework for Live Image Transformation (FLITr)
+ * Copyright (c) 2010 CSIR
+ *
+ * This file is part of FLITr.
+ *
+ * FLITr is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * FLITr is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FLITr. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FIP_DPT_H
+#define FIP_DPT_H 1
+
+#include <flitr/image_processor.h>
+
+namespace flitr {
+    
+    struct Node
+    {
+    public:
+        uint32_t index_;
+        uint8_t value_;
+        uint32_t size_;
+        std::vector<uint32_t> arcIndices_;
+        std::vector<uint32_t> pixelIndices_;
+    };
+    
+    struct Arc
+    {
+    public:
+        uint32_t index_;
+        bool active_;
+        uint32_t nodeIndices_[2];
+    };
+    
+    /*! Compute DPT of image. Currently assumes 8-bit mono input! */
+    class FLITR_EXPORT FIPDPT : public ImageProcessor
+    {
+    public:
+        
+        /*! Constructor given the upstream producer.
+         *@param producer The upstream image producer.
+         *@param images_per_slot The number of images per image slot from the upstream producer.
+         *@param buffer_size The size of the shared image buffer of the downstream producer.*/
+        FIPDPT(ImageProducer& upStreamProducer, uint32_t filterPulseSize,
+               uint32_t images_per_slot,
+               uint32_t buffer_size=FLITR_DEFAULT_SHARED_BUFFER_NUM_SLOTS);
+        
+        /*! Virtual destructor */
+        virtual ~FIPDPT();
+        
+        /*! Method to initialise the object.
+         *@return Boolean result flag. True indicates successful initialisation.*/
+        virtual bool init();
+        
+        /*!Synchronous trigger method. Called automatically by the trigger thread if started.
+         *@sa ImageProcessor::startTriggerThread*/
+        virtual bool trigger();
+        
+    private:
+        
+        bool isBump(const size_t nodeIndex)
+        {
+            const Node &node=nodeVect_[nodeIndex];
+            
+            for (const auto arcIndex : node.arcIndices_)
+            {
+                const auto &arc=arcVect_[arcIndex];
+                
+                if (arc.active_)
+                {
+                    const auto neighbourIndex=(arc.nodeIndices_[0]==nodeIndex) ? arc.nodeIndices_[1] : arc.nodeIndices_[0];
+                    
+                    if (nodeVect_[neighbourIndex].value_ >= node.value_)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+        
+        bool isPit(const size_t nodeIndex)
+        {
+            const Node &node=nodeVect_[nodeIndex];
+            
+            for (const auto arcIndex : node.arcIndices_)
+            {
+                const auto &arc=arcVect_[arcIndex];
+                
+                if (arc.active_)
+                {
+                    const auto neighbourIndex=(arc.nodeIndices_[0]==nodeIndex) ? arc.nodeIndices_[1] : arc.nodeIndices_[0];
+                    
+                    if (nodeVect_[neighbourIndex].value_ <= node.value_)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+        
+        int16_t flattenToFirstNeighbour(const size_t nodeIndex)
+        {
+            Node &node=nodeVect_[nodeIndex];
+            
+            size_t firtsNeighbourIndex=nodeIndex;
+            
+            for (const auto arcIndex : node.arcIndices_)
+            {
+                const auto &arc=arcVect_[arcIndex];
+                
+                if (arc.active_)
+                {
+                    firtsNeighbourIndex=(arc.nodeIndices_[0]==nodeIndex) ? arc.nodeIndices_[1] : arc.nodeIndices_[0];
+                    break;
+                }
+            }
+            
+            node.value_=nodeVect_[firtsNeighbourIndex].value_;
+            
+            return node.value_;
+        }
+        
+        uint8_t flattenToNearestNeighbour(const size_t nodeIndex)
+        {
+            Node &node=nodeVect_[nodeIndex];
+            
+            size_t nearestNeighbourIndex=nodeIndex;
+            
+            for (const auto arcIndex : node.arcIndices_)
+            {
+                const auto &arc=arcVect_[arcIndex];
+                
+                if (arc.active_)
+                {
+                    const auto neighbourIndex=(arc.nodeIndices_[0]==nodeIndex) ? arc.nodeIndices_[1] : arc.nodeIndices_[0];
+                    
+                    if (( nearestNeighbourIndex==nodeIndex ) ||
+                        ( abs(int(nodeVect_[neighbourIndex].value_)-int(node.value_)) <= abs(int(nodeVect_[nearestNeighbourIndex].value_)-int(node.value_)) ))
+                    {
+                        nearestNeighbourIndex=neighbourIndex;
+                    }
+                }
+            }
+            
+            node.value_=nodeVect_[nearestNeighbourIndex].value_;
+            
+            return node.value_;
+        }
+        
+        void updatePotentiallyActiveNodeIndexVect()
+        {
+            potentiallyActiveNodeIndexVect_.clear();
+            
+            for (const auto & node : nodeVect_)
+            {
+                if (node.size_>0)
+                {
+                    potentiallyActiveNodeIndexVect_.push_back(node.index_);
+                }
+            }
+        }
+        
+        size_t mergeArc(const uint32_t arcIndex);
+        size_t mergeFromAll();
+        size_t mergeFromList(const std::vector<uint32_t> nodeIndicesToMerge);
+        
+        
+    private:
+        uint32_t filterPulseSize_;
+        
+        std::vector<Node> nodeVect_;
+        std::vector<size_t> potentiallyActiveNodeIndexVect_;
+        
+        std::vector<Arc> arcVect_;
+    };
+    
+}
+
+#endif //FIP_DPT_H
