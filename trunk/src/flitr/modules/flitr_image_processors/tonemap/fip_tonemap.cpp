e6b5d0a0191fb51ff7c644675c8ab150cbcedae9
bernardt.duvenhage <bernardt.duvenhage@7590bd86-b2cf-772d-1646-1d1f3f83a1b0>
2014-11-18 15:42:49 +0000

    Updated the CPU tonemapping pass to support rgb8 images in addition to float32 images.

--- trunk/src/flitr/modules/flitr_image_processors/tonemap/fip_tonemap.cpp
+++ trunk/src/flitr/modules/flitr_image_processors/tonemap/fip_tonemap.cpp
@@ -26,15 +26,15 @@ using std::shared_ptr;
 FIPTonemap::FIPTonemap(ImageProducer& upStreamProducer, uint32_t images_per_slot,
                        float power,
                        uint32_t buffer_size) :
-ImageProcessor(upStreamProducer, images_per_slot, buffer_size),
-power_(power)
+    ImageProcessor(upStreamProducer, images_per_slot, buffer_size),
+    power_(power)
 {
-    
+
     //Setup image format being produced to downstream.
     for (uint32_t i=0; i<images_per_slot; i++) {
         ImageFormat_.push_back(upStreamProducer.getFormat(i));//Output format is same as input format.
     }
-    
+
 }
 
 FIPTonemap::~FIPTonemap()
@@ -45,26 +45,26 @@ bool FIPTonemap::init()
 {
     bool rValue=ImageProcessor::init();
     //Note: SharedImageBuffer of downstream producer is initialised with storage in ImageProcessor::init.
-    
+
     size_t maxHeight=0;
-    
+
     for (uint32_t i=0; i<ImagesPerSlot_; i++)
     {
         const ImageFormat imFormat=getUpstreamFormat(i);//Downstream format is same as upstream format.
         //const ImageFormat::PixelFormat pixelFormat=imFormat.getPixelFormat();
-        
+
         //const size_t width=imFormat.getWidth();
         const size_t height=imFormat.getHeight();
-        
+
         //const size_t componentsPerPixel=imFormat.getComponentsPerPixel();
         //const size_t bytesPerPixel=imFormat.getBytesPerPixel();
-        
+
         if (height>maxHeight)
         {
             maxHeight=height;
         }
     }
-    
+
     return rValue;
 }
 
@@ -74,49 +74,67 @@ bool FIPTonemap::trigger()
     {//There are images to consume and the downstream producer has space to produce.
         std::vector<Image**> imvRead=reserveReadSlot();
         std::vector<Image**> imvWrite=reserveWriteSlot();
-        
+
         //Start stats measurement event.
         ProcessorStats_->tick();
-        
+
         for (size_t imgNum=0; imgNum<ImagesPerSlot_; imgNum++)
         {
-            Image const * const imRead = *(imvRead[imgNum]);
-            Image * const imWrite = *(imvWrite[imgNum]);
-            
-            float const * const dataRead=(float const * const)imRead->data();
-            float * const dataWrite=(float * const )imWrite->data();
-            
             const ImageFormat imFormat=getUpstreamFormat(imgNum);//Downstream format is same as upstream format.
-            
+
             const size_t width=imFormat.getWidth();
             const size_t height=imFormat.getHeight();
-            
-            size_t y=0;
-            {
+            const size_t bytesPerPixel=imFormat.getBytesPerPixel();
+
+            Image const * const imRead = *(imvRead[imgNum]);
+            Image * const imWrite = *(imvWrite[imgNum]);
+
+            if (imFormat.getPixelFormat()==flitr::ImageFormat::FLITR_PIX_FMT_Y_F32)
+            {//Image format float32.
+                float const * const dataRead=(float const * const)imRead->data();
+                float * const dataWrite=(float * const )imWrite->data();
+
+                for (size_t y=0; y<height; ++y)
                 {
-                    for (y=0; y<height; ++y)
+                    const size_t lineOffset=y * width;
+
+                    for (size_t x=0; x<width; ++x)
                     {
-                        const size_t lineOffset=y * width;
-                        
-                        for (size_t x=1; x<width; ++x)
-                        {
-                            dataWrite[lineOffset + x]=powf(dataRead[lineOffset + x], power_);
-                        }
+                        dataWrite[lineOffset + x]=powf(dataRead[lineOffset + x], power_);
+                    }
+                }
+            } else
+            if (imFormat.getPixelFormat()==flitr::ImageFormat::FLITR_PIX_FMT_RGB_8)
+            {//Image format rgb8.
+                uint8_t const * const dataRead=(uint8_t const * const)imRead->data();
+                uint8_t * const dataWrite=(uint8_t * const )imWrite->data();
+
+                for (size_t y=0; y<height; ++y)
+                {
+                    const size_t lineOffset=(y * width)*bytesPerPixel;
+                    size_t pixelOffset=0;
+
+                    for (size_t x=0; x<width; ++x)
+                    {
+                        dataWrite[lineOffset + pixelOffset + 0]=uint8_t(powf(float(dataRead[lineOffset + pixelOffset + 0])*(1.0f/255.0f), power_)*255.0f+0.5f);
+                        dataWrite[lineOffset + pixelOffset + 1]=uint8_t(powf(float(dataRead[lineOffset + pixelOffset + 1])*(1.0f/255.0f), power_)*255.0f+0.5f);
+                        dataWrite[lineOffset + pixelOffset + 2]=uint8_t(powf(float(dataRead[lineOffset + pixelOffset + 2])*(1.0f/255.0f), power_)*255.0f+0.5f);
+                        pixelOffset+=bytesPerPixel;
                     }
                 }
             }
         }
-        
-        
+
+
         //Stop stats measurement event.
         ProcessorStats_->tock();
-        
+
         releaseWriteSlot();
         releaseReadSlot();
-        
+
         return true;
     }
-    
+
     return false;
 }
 
