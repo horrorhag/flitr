7319b0d503c9875771f9e215a9329473a0dd940f
bduvenhage <bernardt.duvenhage@gmail.com>
2016-03-23 09:02:40 +0200

    Added, but commented out, ffmpeg's libresample that is required for more recent versions of ffmpeg.

--- CMakeModules/FindFFmpeg.cmake
+++ CMakeModules/FindFFmpeg.cmake
@@ -212,6 +212,7 @@ FFmpeg_FIND(LIBAVDEVICE avdevice avdevice.h)
 FFmpeg_FIND(LIBAVCODEC  avcodec  avcodec.h)
 FFmpeg_FIND(LIBAVUTIL   avutil   avutil.h)
 FFmpeg_FIND(LIBSWSCALE  swscale  swscale.h)
+#FFmpeg_FIND(LIBSWRESAMPLE  swresample  swresample.h)
 
 
 MARK_AS_ADVANCED(CLEAR FFmpeg_LIBAVFORMAT_LIBRARIES) #Mark one lib as non-advanced to show where FFmpeg was found.
@@ -226,6 +227,7 @@ IF(FFmpeg_LIBAVFORMAT_FOUND AND FFmpeg_LIBAVDEVICE_FOUND AND FFmpeg_LIBAVCODEC_F
         ${FFmpeg_LIBAVCODEC_INCLUDE_DIRS} #${FFmpeg_LIBAVCODEC_INCLUDE_DIRS}/libavcodec
         ${FFmpeg_LIBAVUTIL_INCLUDE_DIRS} #${FFmpeg_LIBAVUTIL_INCLUDE_DIRS}/libavutil
         ${FFmpeg_LIBSWSCALE_INCLUDE_DIRS} #${FFmpeg_LIBSWSCALE_INCLUDE_DIRS}/libswscale
+#        ${FFmpeg_LIBSWRESAMPLE_INCLUDE_DIRS} #${FFmpeg_LIBSWRESAMPLE_INCLUDE_DIRS}/libswresample
         #CACHE STRING  "docstring"
     )
 
@@ -238,6 +240,7 @@ IF(FFmpeg_LIBAVFORMAT_FOUND AND FFmpeg_LIBAVDEVICE_FOUND AND FFmpeg_LIBAVCODEC_F
             #${FFmpeg_STDINT_INCLUDE_DIR}/libavcodec
             #${FFmpeg_STDINT_INCLUDE_DIR}/libavutil
             #${FFmpeg_STDINT_INCLUDE_DIR}/libswscale
+            #${FFmpeg_STDINT_INCLUDE_DIR}/libswresample
             #CACHE  STRING  "docstring"
         )
     ENDIF()
@@ -248,6 +251,7 @@ IF(FFmpeg_LIBAVFORMAT_FOUND AND FFmpeg_LIBAVDEVICE_FOUND AND FFmpeg_LIBAVCODEC_F
         ${FFmpeg_LIBAVCODEC_LIBRARIES}
         ${FFmpeg_LIBAVUTIL_LIBRARIES}
         ${FFmpeg_LIBSWSCALE_LIBRARIES}
+#        ${FFmpeg_LIBSWRESAMPLE_LIBRARIES}
         #CACHE  STRING  "docstring"
     )
 
