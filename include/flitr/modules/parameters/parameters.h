98efa7c3ee0614158fb0cf5d2e5f31dc40c2e7e6
Carel Jnr Combrink <carel.combrink@gmail.com>
2016-05-26 11:32:31 +0200

    Removed compiler warnings of unused variables

--- include/flitr/modules/parameters/parameters.h
+++ include/flitr/modules/parameters/parameters.h
@@ -48,31 +48,31 @@ public:
 
     virtual int getNumberOfParms() { return 0; }
 
-    virtual EParmType getParmType(int id) { return PARM_UNDF;}
-    virtual std::string getParmName(int id) { return std::string("PARM_UNDF");}
+    virtual EParmType getParmType(int id) { (void)id; return PARM_UNDF;}
+    virtual std::string getParmName(int id) { (void)id; return std::string("PARM_UNDF");}
 
     virtual std::string getTitle() { return "Title"; }
 
-    virtual int getEnum(int id) { return 0; }
-    virtual std::string getEnumText(int id, int v) { return "Default"; }
-    virtual int getInt(int id) { return 0; }
-    virtual float getFloat(int id) { return 0.f; }
-    virtual double getDouble(int id) { return 0.0; }
-    virtual bool getBool(int id) { return false; }
+    virtual int getEnum(int id) { (void)id; return 0; }
+    virtual std::string getEnumText(int id, int v) { (void)id; (void)v; return "Default"; }
+    virtual int getInt(int id) { (void)id; return 0; }
+    virtual float getFloat(int id) { (void)id; return 0.f; }
+    virtual double getDouble(int id) { (void)id; return 0.0; }
+    virtual bool getBool(int id) { (void)id; return false; }
 
-    virtual bool getEnumRange(int id, int &low, int &high) { return false; }
-    virtual bool getIntRange(int id, int &low, int &high) { return false; }
-    virtual bool getFloatRange(int id, float &low, float &high) { return false; }
-    virtual bool getDoubleRange(int id, double &low, double &high) { return false; }
+    virtual bool getEnumRange(int id, int &low, int &high) { (void)id; (void)low; (void)high; return false; }
+    virtual bool getIntRange(int id, int &low, int &high) { (void)id; (void)low; (void)high; return false; }
+    virtual bool getFloatRange(int id, float &low, float &high) { (void)id; (void)low; (void)high; return false; }
+    virtual bool getDoubleRange(int id, double &low, double &high) { (void)id; (void)low; (void)high; return false; }
 
-    virtual bool setEnum(int id, int v) { return false; }
-    virtual bool setInt(int id, int v) { return false; }
-    virtual bool setFloat(int id, float v) { return false; }
-    virtual bool setDouble(int id, double v) { return false; }
-    virtual bool setBool(int id, bool v) { return false; }
+    virtual bool setEnum(int id, int v) { (void)id; (void)v; return false; }
+    virtual bool setInt(int id, int v) { (void)id; (void)v; return false; }
+    virtual bool setFloat(int id, float v) { (void)id; (void)v; return false; }
+    virtual bool setDouble(int id, double v) { (void)id; (void)v; return false; }
+    virtual bool setBool(int id, bool v) { (void)id; (void)v; return false; }
 
     // TODO: these need to be moved to a image processing process interface class or something like that
-    virtual void enable(bool state=true) {}
+    virtual void enable(bool state=true) { (void)state; }
     virtual bool isEnabled() { return false; }
     virtual void preFrame() {}
     virtual void postFrame() {}
